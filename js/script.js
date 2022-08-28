fd5ec27fb95aa74e8f7ea7564040a8a93ad8bf97
mochfarrel06 <mochfarrel2606@gmail.com>
2022-08-28 22:57:11 +0700

    membuat navbar dan halaman home

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Scroll navbar
+function scrollNavbar() {
+  const navbar = document.getElementById('navbar');
+
+  if (this.scrollY >= 100) {
+    navbar.classList.add('scroll-navbar');
+  } else {
+    navbar.classList.remove('scroll-navbar');
+  }
+}
+
+window.addEventListener('scroll', scrollNavbar);
+
+// Menu mobile
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.getElementById('menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navbar.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navbar.classList.remove('active');
+  })
+);
